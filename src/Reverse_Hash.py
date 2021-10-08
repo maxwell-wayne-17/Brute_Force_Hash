e4be9828aca98086305face3303d6591ed14bd67
Maxwell Wayne <maxwell.wayne.17@cnu.edu>
2021-10-07 23:14:35 -0400

    Initial commit

---
+++ src/Reverse_Hash.py
@@ -0,0 +1,33 @@
+import hashlib
+import itertools
+
+def crack_3_upper_case(og_hash):
+    string = 'AAA'
+    char_list = []
+    perm = ""
+    b = bytes(string, 'utf-8')
+    #print(hashlib.md5(b).hexdigest())
+
+    if og_hash == hashlib.md5(b).hexdigest():
+        print(b)
+        return b
+
+    # Go through length 3
+    for x in range(b[0], b[0] + 26):
+        for y in range(b[1], b[1] + 26):
+            for z in range(b[2], b[2] + 26):
+                combo = chr(x) + chr(y) + chr(z)
+                combo_hash = hashlib.md5(combo.encode())
+                if (combo_hash.hexdigest() == og_hash):
+                    print ("found + ", combo)
+                    return combo
+                #print(combo)
+
+
+
+
+test_hash = 'AAA'
+b = bytes(test_hash, 'utf-8')
+result = hashlib.md5(test_hash.encode())
+bytes_result = hashlib.md5(b)
+crack_3_upper_case( "b05815e0fb5626ccb478b49a4e85d3a8" )
